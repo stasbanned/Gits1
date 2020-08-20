26404c461dd9854c68266acac070f1ff66bd3245
Stanislav <stasbanned@gmail.com>
2020-08-20 19:15:30 +0300

    hi

---
+++ Gits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gits
+//
+//  Created by Станислав Тищенко on 20.08.2020.
+//  Copyright © 2020 Станислав Тищенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("SSSS")
+    }
+
+}
+
